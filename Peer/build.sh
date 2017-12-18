7b582389e3024269c7453034ac67de8bec62de7c
Alexander Steen <ale.steen@hotmail.com>
2017-12-18 15:13:03 +0100

    Fixed docker problem

--- Peer/build.sh
+++ Peer/build.sh
@@ -1,3 +1,8 @@
-mvn clean
-mvn install
-docker build -t peer .
+mvn clean package docker:build
+
+set +x
+docker login -u databasserne -p ultrapassword
+set -x
+
+docker push databasserne/block-chain
+docker logout
